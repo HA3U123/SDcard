0b07013077c88c18ec8965b7253ef23a3bd7db0d
HA3U123 <krasnovnazar008@gmail.com>
2026-08-19 12:20:54 +0300

    Update sd.ts

--- sd.ts
+++ sd.ts
@@ -1,41 +1,18 @@
-/**
- * Блоки для работы с SD-картой (FAT32) на micro:bit v2.
- * Использует аппаратный интерфейс SPI.
- */
-//% color=#1E6273 icon="\uf0a0" block="Skrepka SD" weight=95
-namespace SkrepkaSD {
-
     /**
      * Инициализирует SD-карту с указанием пинов подключения SPI.
-     * @param mosi Pin для передачи данных (Master Out Slave In), eg: DigitalPin.P15
-     * @param miso Pin для приема данных (Master In Slave Out), eg: DigitalPin.P14
-     * @param sck Pin тактового сигнала (Serial Clock), eg: DigitalPin.P13
-     * @param cs Pin выбора чипа (Chip Select), eg: DigitalPin.P16
+     * @param mosi Pin для передачи данных, eg: DigitalPin.P15
+     * @param miso Pin для приема данных, eg: DigitalPin.P14
+     * @param sck Pin тактового сигнала, eg: DigitalPin.P13
+     * @param cs Pin выбора чипа, eg: DigitalPin.P16
      */
     //% blockId="skrepka_sd_init_block" 
     //% block="Инициализировать SD-карту| MOSI %mosi| MISO %miso| SCK %sck| CS %cs"
-    //% mosi.fieldEditor="gridpicker" mosi.fieldOptions.columns=4
-    //% miso.fieldEditor="gridpicker" miso.fieldOptions.columns=4
-    //% sck.fieldEditor="gridpicker" sck.fieldOptions.columns=4
-    //% cs.fieldEditor="gridpicker" cs.fieldOptions.columns=4
+    //% mosi.defl=DigitalPin.P15 miso.defl=DigitalPin.P14 sck.defl=DigitalPin.P13 cs.defl=DigitalPin.P16
     //% weight=100
     export function initialize(mosi: DigitalPin, miso: DigitalPin, sck: DigitalPin, cs: DigitalPin): void {
-        // Перенаправляем вызов в C++, приводя перечисление DigitalPin к числу (ID пина)
         initCard(mosi, miso, sck, cs);
     }
-
-    /**
-     * Открывает файл для пошагового чтения строк.
-     * @param filename Имя файла на SD-карте (например, "data.txt"), eg: "log.txt"
-     */
-    //% blockId="skrepka_sd_open_block" 
-    //% block="Открыть файл %filename для чтения"
-    //% filename.shadow="text"
-    //% weight=90
-    export function openFile(filename: string): boolean {
-        return openGizFile(filename);
-    }
-
+    
     /**
      * Считывает следующую строку из ранее открытого файла.
      * Если достигнут конец файла (EOF), автоматически закрывает его и возвращает "EOF".
