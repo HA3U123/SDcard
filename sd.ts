0ee8afa0b99586d8424c088c48cfd001d7126620
HA3U123 <krasnovnazar008@gmail.com>
2026-08-19 12:09:48 +0300

    Update sd.ts

--- sd.ts
+++ sd.ts
@@ -1,40 +1,77 @@
-//% color=#4c97ff icon="\uf0a0" block="Skrepka SD"
+/**
+ * Блоки для работы с SD-картой (FAT32) на micro:bit v2.
+ * Использует аппаратный интерфейс SPI.
+ */
+//% color=#1E6273 icon="\uf0a0" block="Skrepka SD" weight=95
 namespace SkrepkaSD {
 
     /**
-     * Инициализация SD карты с выбором пинов.
+     * Инициализирует SD-карту с указанием пинов подключения SPI.
+     * @param mosi Pin для передачи данных (Master Out Slave In), eg: DigitalPin.P15
+     * @param miso Pin для приема данных (Master In Slave Out), eg: DigitalPin.P14
+     * @param sck Pin тактового сигнала (Serial Clock), eg: DigitalPin.P13
+     * @param cs Pin выбора чипа (Chip Select), eg: DigitalPin.P16
      */
-    //% block="Инициализировать карту: MOSI %mosi| MISO %miso| SCK %sck| CS %cs"
-    //% mosi.shadow="digitalioPins"
-    //% miso.shadow="digitalioPins"
-    //% sck.shadow="digitalioPins"
-    //% cs.shadow="digitalioPins"
-    export function init(mosi: number, miso: number, sck: number, cs: number): void {
-        // Вызываем функцию из C++ через shim
+    //% blockId="skrepka_sd_init_block" 
+    //% block="Инициализировать SD-карту| MOSI %mosi| MISO %miso| SCK %sck| CS %cs"
+    //% mosi.fieldEditor="gridpicker" mosi.fieldOptions.columns=4
+    //% miso.fieldEditor="gridpicker" miso.fieldOptions.columns=4
+    //% sck.fieldEditor="gridpicker" sck.fieldOptions.columns=4
+    //% cs.fieldEditor="gridpicker" cs.fieldOptions.columns=4
+    //% weight=100
+    export function initialize(mosi: DigitalPin, miso: DigitalPin, sck: DigitalPin, cs: DigitalPin): void {
+        // Перенаправляем вызов в C++, приводя перечисление DigitalPin к числу (ID пина)
         initCard(mosi, miso, sck, cs);
     }
 
     /**
-     * Открыть файл для чтения.
+     * Открывает файл для пошагового чтения строк.
+     * @param filename Имя файла на SD-карте (например, "data.txt"), eg: "log.txt"
      */
+    //% blockId="skrepka_sd_open_block" 
     //% block="Открыть файл %filename для чтения"
+    //% filename.shadow="text"
+    //% weight=90
     export function openFile(filename: string): boolean {
         return openGizFile(filename);
     }
 
     /**
-     * Прочитать одну строку из открытого файла.
+     * Считывает следующую строку из ранее открытого файла.
+     * Если достигнут конец файла (EOF), автоматически закрывает его и возвращает "EOF".
      */
-    //% block="Прочитать строку"
+    //% blockId="skrepka_sd_read_block" 
+    //% block="Прочитать строку из файла"
+    //% weight=80
     export function readLine(): string {
         return readToBuffer();
     }
 
     /**
-     * Записать текст в файл.
+     * Проверяет, осталась ли еще информация в файле (не равен ли результат "EOF").
+     * Полезно для циклов "пока не конец файла".
+     * @param line Переменная строки, полученная из файла
      */
-    //% block="Записать в файл %filename текст %text"
-    export function write(filename: string, text: string): boolean {
+    //% blockId="skrepka_sd_is_eof" 
+    //% block="строка %line является концом файла (EOF)?"
+    //% line.shadow="text"
+    //% weight=75
+    export function isEOF(line: string): boolean {
+        return line == "EOF";
+    }
+
+    /**
+     * Дописывает текстовую строку в конец указанного файла (режим Append).
+     * Если файл не существовал, он будет создан автоматически.
+     * Каждая запись автоматически завершается переносом строки (\n).
+     * @param filename Имя файла, eg: "log.txt"
+     * @param text Текст для записи, eg: "Привет, мир!"
+     */
+    //% blockId="skrepka_sd_write_block" 
+    //% block="В файл %filename записать строку %text"
+    //% filename.shadow="text" text.shadow="text"
+    //% weight=70
+    export function writeLine(filename: string, text: string): boolean {
         return writeToFile(filename, text);
     }
 }
