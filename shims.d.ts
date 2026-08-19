03eff36d91ffd296e160082aeec0ee784d4cbce5
HA3U123 <krasnovnazar008@gmail.com>
2026-08-20 01:56:55 +0300

    Update shims.d.ts

--- shims.d.ts
+++ shims.d.ts
@@ -1,14 +1,14 @@
 //% color=#1E6273 icon="\uf0a0" block="Skrepka SD" weight=95
 namespace SkrepkaSD {
     //% shim=SkrepkaSD::initCard
-    function initCard(mosi: number, miso: number, sck: number, cs: number): void;
+    export function initCard(mosi: number, miso: number, sck: number, cs: number): void;
 
     //% shim=SkrepkaSD::openGizFile
-    function openGizFile(filename: string): boolean;
+    export function openGizFile(filename: string): boolean;
 
     //% shim=SkrepkaSD::readToBuffer
-    function readToBuffer(): string;
+    export function readToBuffer(): string;
 
     //% shim=SkrepkaSD::writeToFile
-    function writeToFile(filename: string, text: string): boolean;
+    export function writeToFile(filename: string, text: string): boolean;
 }
