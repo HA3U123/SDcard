2c03a335b2f2c9d02b086d7a79e12e1adf12072b
HA3U123 <krasnovnazar008@gmail.com>
2026-08-20 01:41:46 +0300

    Update sdcard.cpp

--- sdcard.cpp
+++ sdcard.cpp
@@ -9,7 +9,6 @@ namespace SkrepkaSD {
     mbed::FATFileSystem* fs = nullptr;
     FILE* activeFile = nullptr;
 
-    // Внутренняя функция для безопасного закрытия файла
     void closeActiveFile() {
         if (activeFile != nullptr) {
             fclose(activeFile);
@@ -17,12 +16,9 @@ namespace SkrepkaSD {
         }
     }
 
-        //% blockId="skrepka_sd_init" block="Инициализировать SD-карту: MOSI %mosi| MISO %miso| SCK %sck| CS %cs"
-    //% weight=100
     void initCard(int mosi, int miso, int sck, int cs) {
         if (sd != nullptr) return; 
 
-        // Получаем CODAL-объекты пинов по их ID из MakeCode
         MicroBitPin* p_mosi = pxt::getPin(mosi);
         MicroBitPin* p_miso = pxt::getPin(miso);
         MicroBitPin* p_sck  = pxt::getPin(sck);
@@ -30,7 +26,6 @@ namespace SkrepkaSD {
 
         if (!p_mosi || !p_miso || !p_sck || !p_cs) return;
 
-        // ИСПРАВЛЕНО: Явное приведение типов uint16_t (из CODAL) к PinName (для mbed OS)
         PinName mosi_p = (PinName)p_mosi->name;
         PinName miso_p = (PinName)p_miso->name;
         PinName sck_p  = (PinName)p_sck->name;
@@ -46,14 +41,11 @@ namespace SkrepkaSD {
         }
     }
 
-    //% blockId="skrepka_sd_open" block="Открыть файл %filename для чтения"
-    //% weight=90
     bool openGizFile(StringData* filename) {
         if (!fs || filename == nullptr) return false; 
         closeActiveFile(); 
         
         String name(filename);
-        // ИСПРАВЛЕНО: Буфер увеличен до 128 байт для безопасной работы с путями
         char path[128];
         snprintf(path, sizeof(path), "/sd/%s", name.data());
         
@@ -61,18 +53,15 @@ namespace SkrepkaSD {
         return (activeFile != nullptr);
     }
 
-    //% blockId="skrepka_sd_read" block="Прочитать строку из файла"
-    //% weight=80
     StringData* readToBuffer() {
         if (activeFile == nullptr) {
             return pxt::mkString("EOF");
         }
         
-        char lineBuffer[128]; // Буфер для одной строки (можно увеличить при необходимости)
+        char lineBuffer[128]; 
         int index = 0;
         int c;
         
-        // Оставляем 1 байт под терминальный нуль
         while (index < (sizeof(lineBuffer) - 1)) {
             c = fgetc(activeFile);
             
@@ -87,7 +76,6 @@ namespace SkrepkaSD {
             if (c == '\r') {
                 int next = fgetc(activeFile);
                 if (next != '\n' && next != EOF) {
-                    // ИСПРАВЛЕНО: Вместо fseek используем стандартный ungetc
                     ungetc(next, activeFile);
                 }
                 break; 
@@ -103,8 +91,6 @@ namespace SkrepkaSD {
         return pxt::mkString(lineBuffer);
     }
 
-    //% blockId="skrepka_sd_write" block="Записать в файл %filename текст %text"
-    //% weight=70
     bool writeToFile(StringData* filename, StringData* text) {
         if (!fs || filename == nullptr || text == nullptr) return false;
 
@@ -113,7 +99,6 @@ namespace SkrepkaSD {
         String name(filename);
         String content(text);
         
-        // ИСПРАВЛЕНО: Буфер увеличен до 128 байт
         char path[128];
         snprintf(path, sizeof(path), "/sd/%s", name.data());
 
