7f7582d8b0b2f4ba7dce0e13ada944cd948e06e1
HA3U123 <krasnovnazar008@gmail.com>
2026-08-19 12:07:52 +0300

    Update shims.d.ts

--- shims.d.ts
+++ shims.d.ts
@@ -1,15 +1,26 @@
-//% color=#4c97ff icon="\uf0a0"
+//% color=#2693ab icon="\uf0a0" block="Skrepka SD"
 namespace SkrepkaSD {
-
+    /**
+     * Инициализирует SD карту через указанные пины.
+     */
     //% shim=SkrepkaSD::initCard
     function initCard(mosi: number, miso: number, sck: number, cs: number): void;
 
+    /**
+     * Открывает файл на чтение. Возвращает true при успехе.
+     */
     //% shim=SkrepkaSD::openGizFile
     function openGizFile(filename: string): boolean;
 
+    /**
+     * Читает одну строку из открытого файла. Возвращает "EOF", если файл закончился.
+     */
     //% shim=SkrepkaSD::readToBuffer
     function readToBuffer(): string;
 
+    /**
+     * Дописывает строку в файл. Автоматически переносит строку в конце.
+     */
     //% shim=SkrepkaSD::writeToFile
     function writeToFile(filename: string, text: string): boolean;
 }
